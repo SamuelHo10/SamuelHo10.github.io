ef5724ba47c9e110098636d1245ad5f0414f1a11
Samuel Ho <samuelho613@gmail.com>
2025-01-11 17:19:19 -0500

    updated website

--- scripts/load_projects.js
+++ scripts/load_projects.js
@@ -1,14 +1,14 @@
 var projects = [
     { name: "SystemVerilog Fluid Simulation", image: "./img/fluid_simulation.png", link: "./projects/fluid_verilog.html", year: 2025, tags: ["Hobby Project", "Computer Engineering"] },
     { name: "LED Cube", image: "./img/led_cube.png", link: "./projects/led_cube.html", year: 2022, tags: ["Hobby Project", "Electrical Engineering", "Computer Engineering"] },
-    { name: "2D Rigidbody Physics Engine", image: "./img/physics_engine.jpg", link: "./projects/physics_engine.html", year: 2022, tags: ["Hobby Project", "Programming", "Highschool Project"] },
+    { name: "2D Rigidbody Physics Engine", image: "./img/physics_engine.jpg", link: "./projects/physics_engine.html", year: 2022, tags: ["Hobby Project", "Physics", "Highschool Project"] },
     { name: "Praxis I Project: Plant Humidifier", image: "./img/praxis_I.png", link: "./projects/praxis_I.html", year: 2023, tags: ["Course Project"] },
     { name: "Praxis II Project: Worm Wheel 360", image: "./img/praxis_II.png", link: "./projects/praxis_II.html", year: 2024, tags: ["Course Project"] },
     { name: "Matboard Girder Bridge", image: "./img/civ_bridge.png", link: "./projects/civ_bridge.html", year: 2023, tags: ["Course Project", "Civil Engineering"] },
     { name: "Winnik Lab Research", image: "./img/fret.jpg", link: "./projects/winnik_lab.html", year: 2024, tags: ["Research Project", "Chemistry"] },
     { name: "UTRA SUMO Robot", image: "./img/utra_sumo.jpg", link: "./projects/utra_sumo.html", year: 2024, tags: ["Club Project", "Robotics"] },        
-    { name: "Trebuchet", image: "./img/trebuchet.jpg", link: "./projects/trebuchet.html", year: 2022, tags: ["Highschool Project"] },
-    { name: "Font Recognition AI", image: "./img/matcha_font.png", link: "./projects/matcha_font.html", year: 2024, tags: ["Course Project", "Programming"] },
+    { name: "Trebuchet", image: "./img/trebuchet.jpg", link: "./projects/trebuchet.html", year: 2022, tags: ["Highschool Project", "Physics"] },
+    { name: "Font Recognition AI", image: "./img/matcha_font.png", link: "./projects/matcha_font.html", year: 2024, tags: ["Course Project", "Machine Learning"] },
     { name: "Sullan Lab Research", image: "./img/microfluidics.jpg", link: "./projects/sullan_lab.html", year: 2024, tags: ["Research Project", "Chemistry"] },
 ];
 
